506e16584a51617ef024616c3b93123d55980120
MayuriGanore <115164210+MayuriGanore@users.noreply.github.com>
2024-07-05 16:15:24 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Hello = () => {
+  return <h2>Hello Page</h2>;
 }
 
-export default App;
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/hello" element={<Hello />} />
+      </Routes>
+      <h1>Hello World!!</h1>
+    </div>
+  )
+}
+
+export default App
